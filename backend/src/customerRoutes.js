e744429e27cd914d1e6848fabd166698f789c726
abhishekchaurasiya <abhichek2580@gmail.com>
2025-01-21 12:21:36 +0530

    Add customer routers

---
+++ backend/src/customerRoutes.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const customerRouter = express.Router();
+
+export default customerRouter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import connectDatabase from "./config/database.js";
 import configVariable from "./config/config.js";
+import customerRouter from "./customerRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api/customers", customerRouter);
 
 const start = async () => {
   await connectDatabase();
