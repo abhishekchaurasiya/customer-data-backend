8d3b2829382a405a7c983d6486fc2a0be1bd8822
abhishekchaurasiya <abhichek2580@gmail.com>
2025-01-22 17:54:14 +0530

    configure file add

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -5,6 +5,7 @@ const configVariable = {
   databaseUrl: process.env.MONGODB_URI,
   port: process.env.PORT,
   dbName: process.env.DB_NAME,
+  collectionName: process.env.COLLECTION,
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
   redisPassword: process.env.REDIS_PASSWORD,
